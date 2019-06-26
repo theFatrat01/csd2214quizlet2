bdced102d8cc20f3565e06f564082232ea8dc268
Ashlin Innis <754360@student.tor.loc>
2019-06-26 14:19:38 -0400

    2.15

---
+++ javascriptfile1.js
@@ -0,0 +1,8 @@
+function waitingfn(timeinms){
+    const futuretime=Date.now() =timeinms;
+    while (futuretime > Date.now()){
+        
+    }
+}
+waitingfn(3000);
+console.log("fuction call has just ended");
